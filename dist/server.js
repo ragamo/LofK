1d1f1ce54311a6d8f2a980573aac5867abba9daf
Christian Haack <christian@chipax.com>
2020-07-15 11:42:36 -0400

    removed dist

--- dist/server.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./dotenv");
-const Lofk_1 = require("./core/Lofk");
-Lofk_1.server('discord');
-//# sourceMappingURL=server.js.map
