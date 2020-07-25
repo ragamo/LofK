1788837aeacf4216f8b3f8e556e35e20be912455
Christian Haack <christian@chipax.com>
2020-07-25 16:10:05 -0400

    added healtcheck server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,14 @@
 import './dotenv';
+import express from 'express';
 import Lofk from './core/Lofk';
 
-new Lofk(['discord']);
+// Healthcheck
+const httpServer = express();
+httpServer.get('/', (req, res) => {
+  return res.status(200).end();
+});
+
+httpServer.listen(8080, '0.0.0.0', () => {
+  console.log('🌎 HTTP server started on 0.0.0.0:8080');
+  new Lofk(['discord']);
+});
